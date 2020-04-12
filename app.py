421d68d398c1fc4be8b66c92143490510108e0e9
Tom Hall <thomas.hall@silvercar.com>
2020-04-12 14:07:02 -0500

    working example

--- app.py
+++ app.py
@@ -2,10 +2,14 @@
 
 from aws_cdk import core
 
-from python_static_site.python_static_site_stack import PythonStaticSiteStack
+from python_static_site.python_static_site_stack import MyStack
 
 
 app = core.App()
-PythonStaticSiteStack(app, "python-static-site")
+MyStack(
+    app,
+    "static-site-cdk-2",
+    env=core.Environment(region="us-east-1", account="821330209644"),
+)
 
 app.synth()
--- python_static_site/python_static_site_stack.py
+++ python_static_site/python_static_site_stack.py
@@ -1,9 +1,22 @@
-from aws_cdk import core
+from aws_cdk import (
+    core
+)
+
+from .python_static_site_construct import StaticSiteConstruct, StaticSiteProps
 
 
-class PythonStaticSiteStack(core.Stack):
+class MyStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        props: StaticSiteProps = StaticSiteProps(
+            domain_name=scope.node.try_get_context('domain'),
+            site_sub_domain=scope.node.try_get_context('sub_domain'),
+            hosted_zone_id=scope.node.try_get_context('hosted_zone_id')
+        )
+
+        print(props.domain_name)
+        print(props.site_sub_domain)
+
+        StaticSiteConstruct(self, "MyStaticSiteConstruct", props)
